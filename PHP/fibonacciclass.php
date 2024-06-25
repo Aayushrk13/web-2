0ecc095fc1ec8efa1570d2c867b9f59e0e129c20
Aayush <nccscollege@gmail.com>
2024-06-25 16:22:32 +0545

    fibonacci class

---
+++ PHP/fibonacciclass.php
@@ -0,0 +1,38 @@
+<?php
+    class fibonacci{
+        public $a;
+        public $b;
+        public $n;
+        public $sum=0;
+        function __construct($n){
+            $this->a=0;
+            $this->b=1;
+            $this->n=$n;
+        }
+        function fibo($a,$b){
+            return $a+$b;
+        }
+        function series(){
+            if($this->n==1){
+                echo $this->a;
+	return;
+            }
+            if($this->n==2){
+	echo $this->a."<br>";
+                echo$this->b."<br>";
+	return;
+            }
+	echo "0<br>";
+	echo "1<br>";
+            while($this->n>0){
+                $this->sum=$this->fibo($this->a,$this->b);
+	$this->a=$this->b;
+	$this->b=$this->sum;
+                echo $this->sum."<br>";
+                $this->n--;
+            }
+        }
+    }
+    $obj=new fibonacci(120);
+    $obj->series();
+?>
--- PHP/palindrome.php
+++ PHP/palindrome.php
@@ -1,8 +1,23 @@
 <?php
-    $num="woew";
-    if($num==strrev($num)){
-        echo "$num is palindrome.";
+    $str="asdfdsa";
+    if($str==strrev($str)){
+        echo "$str is palindrome.";
     }else{
-        echo "$num is not a palidrome.";
+        echo "$str is not a palidrome.";
+    }
+    $num=505;
+    $temp=$num;
+    $rev=0;
+    $rev=0;
+    while((int)$num>0){
+        $rem=$num%10;
+        $rev=$rev*10+$rem;
+        $num=$num/10;
+    }
+    echo "<br>";
+    if($temp==$rev){
+        echo "$rev is palindrome.";
+    }else{
+        echo "$temp is not a palindrome.";
     }
 ?>

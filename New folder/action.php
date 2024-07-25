aedc3c695438f4290cdb54dfb18513d452b24b78
Aayush <nccscollege@gmail.com>
2024-07-25 16:24:53 +0545

    form validation using db complete

--- New folder/action.php
+++ New folder/action.php
@@ -1,11 +1,21 @@
 <?php
-    $name="Aayush";
-    $pass="9841379888";
-    $username=$_POST["username"];
-    $password=$_POST["password"];
-    if($name==$username && $password==$pass){
-        header("Location: http://localhost/Aayush-BIM4th/New%20folder/auth.html");
-        exit;
-    }
-    header("Location: http://localhost/Aayush-BIM4th/New%20folder/form.html")
+include("connectiondb.php");//returns $conn
+$username=$_POST["username"];
+$password=$_POST["password"];
+// prevcent mysql injectoin
+$username = stripcslashes($username);  
+$password = stripcslashes($password);  
+$username = mysqli_real_escape_string($conn, $username);  
+$password = mysqli_real_escape_string($conn, $password);  
+
+$checker="select * from employee_info where fullname='$username' and pass='$password'";
+
+$sqloutput=mysqli_query($conn,$checker);
+$count=mysqli_num_rows($sqloutput);
+if($count==1){
+    header("Location: http://localhost/Aayush-BIM4th/New%20folder/homepage.html");
+}else{
+    echo "wrong";
+}
+$conn->close();
 ?>
